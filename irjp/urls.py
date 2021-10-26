932727c09b87294bfa087d2706d1d3c1208350af
TupMiqo <91715967+TupMiqo@users.noreply.github.com>
2021-10-27 00:31:22 +0800

    Update urls.py

--- irjp/urls.py
+++ irjp/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     # read views.py and execute index function
     # read views.py and execute menu function
     path('', views.home, name='Home'),
-    path('reggdb', views.reggdb, name='reggdb'),
     path('register', views.Register, name='Register'),
     path('login2', views.login2, name='login2'),
     path('table', views.table, name='table'),
